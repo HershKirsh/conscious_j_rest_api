a51bffb3462f92168dcd8da656d6c37869776698
HershKirsh <hershkirsh@gmail.com>
2019-11-04 20:10:32 -0500

    little changes

--- data/db.js
+++ data/db.js
@@ -1,2 +1,2 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/consciuos_j', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/consciuos_j', { useUnifiedTopology: true, useNewUrlParser: true });
