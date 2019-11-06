ef066202ba20bfcbbc7852df1db22b11d89d0a72
hershkirsh <hershkirsh@gmail.com>
2019-11-05 22:11:01 -0500

    more CORS handling

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //allow CORS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.header("Access-Control-Allow-Headers", 'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Accept-Encoding, Accept-Language, Referer, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent, Connection, Content-Length, Host');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   next();
 });
